b05635d4733aee14814ebaa02fde9216bd537014
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-06 01:00:12 -0700

    Add proper header documentation

--- wacspace.el
+++ wacspace.el
@@ -1,4 +1,36 @@
-;;; -*- lexical-binding: t -*-
+;;; wacspace.el --- emACS WorkSPACE -*- lexical-binding: t -*-
+
+;; Copyright © 2013 Emanuel Evans
+
+;; Author: Emanuel Evans <emanuel.evans@gmail.com>
+;; Version: 0.1
+;; Created: 26 March 2013
+;; Keywords: workspace
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Provides context-aware workspace management for Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (require 'cl-lib)
 (require 'dash)
@@ -143,3 +175,5 @@ configuration options, see the README."
   (split-window-below))
 
 (provide 'wacspace)
+
+;;; wacspace.el ends here
