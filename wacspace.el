64e02477cbb6a84e9d6a00cb1a7bdc9cd58fbfb7
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-16 22:46:04 -0700

    Version bump: 0.3

--- wacspace.el
+++ wacspace.el
@@ -4,7 +4,7 @@
 
 ;; Author: Emanuel Evans <emanuel.evans@gmail.com>
 ;; URL: http://github.com/shosti/wacspace.el
-;; Version: 0.2
+;; Version: 0.3
 ;; Created: 26 March 2013
 ;; Keywords: workspace
 ;; Package-Requires: ((dash "1.1.0") (cl-lib "0.2"))
