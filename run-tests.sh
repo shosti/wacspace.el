b4837ef7ba4bed4ce3b5b94be4e665c6d264aa24
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-03 08:51:06 -0700

    Get rid of bundled cl-lib

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 ECUKES=$(find elpa/ecukes-*/ecukes | tail -1)
 DASH=$(find elpa/dash-*/dash.el)
 carton exec "$ECUKES" "$@"
-emacs -batch -l ert/cl-lib.el -l ert/ert.el -l ert/unit-tests.el -l $DASH -l wacspace.el -f ert-run-tests-batch-and-exit
+emacs -batch -l ert/ert.el -l ert/unit-tests.el -l $DASH -l wacspace.el -f ert-run-tests-batch-and-exit
