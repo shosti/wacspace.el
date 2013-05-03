404d0cc56c9bbdd9a6254345ae59613b7c983bd0
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-03 09:19:32 -0700

    Update unit test script

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 ECUKES=$(find elpa/ecukes-*/ecukes | tail -1)
 DASH=$(find elpa/dash-*/dash.el)
 carton exec "$ECUKES" "$@"
-emacs -batch -l ert/ert.el -l ert/unit-tests.el -l $DASH -l wacspace.el -f ert-run-tests-batch-and-exit
+emacs -batch -l cl-lib -l ert -l tests/unit-tests.el -l $DASH -l wacspace.el -f ert-run-tests-batch-and-exit
