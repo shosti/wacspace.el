b2f3c762b8ff7f967583c1bfdb6ed0a226cbae86
Emanuel Evans <emanuel.evans@gmail.com>
2014-01-25 21:56:54 -0800

    Upgrade to Cask from Carton

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,5 @@
-ECUKES=$(find elpa/ecukes-*/ecukes | tail -1)
-DASH=$(find elpa/dash-*/dash.el)
-carton exec "$ECUKES" "$@"
+DASH=$(find .cask/*/*/*/dash.el)
+cask exec ecukes --win "$@"
 emacs -batch -l cl-lib -l ert -l tests/unit-tests.el -l $DASH \
     -l wacs-util.el \
     -l wacs-configuration.el \
